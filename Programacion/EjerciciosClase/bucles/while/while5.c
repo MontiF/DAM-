7321225d7bd4e0aafc6891f8205ab6c525f6a5b5
MontiF <miguelmonteagudoferia@gmail.com>
2024-10-29 14:26:04 +0100

    while

--- Programacion/EjerciciosClase/bucles/while/while5.c
+++ Programacion/EjerciciosClase/bucles/while/while5.c
@@ -6,11 +6,15 @@ int main(){
 	int numeroRandom = 7;
 	int num = 0;
 
-
+	
 	while (numeroRandom != num){
-		printf("Adivina el numero: ");
+		printf("Adivina el numero entre el 0 y el 100: ");
 		scanf("%d", &num);
+			if (num < 101){
 			
+			}else{
+				printf("El numero que eligas tiene que ser menor que 100\n");
+			}	
 	} printf("Acertaste");
 	
 	return 0;
