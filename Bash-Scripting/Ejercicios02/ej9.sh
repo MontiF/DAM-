f4f4909b0ea5b72b242205449ec8cf531e2320f3
MontiF <miguelmonteagudoferia@gmail.com>
2025-01-30 21:30:52 +0100

    bash

--- Bash-Scripting/Ejercicios02/ej9.sh
+++ Bash-Scripting/Ejercicios02/ej9.sh
@@ -2,7 +2,13 @@
 
 for SUBDIR in test9/*
 do
-  if [[ $SUBDIR == d ]]; then
-  
+  if [[ -d $SUBDIR ]]; then
+    for archivo in $SUBDIR/*.txt
+    do
+      if [  "$archivo" ]; then
+        head -n 1 "$archivo"  
+        echo " $archivo"
+      fi
+    done
   fi
 done
