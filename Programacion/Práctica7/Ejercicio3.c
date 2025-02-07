02508dbddfc341d42dd69575e1ef17491246da83
MontiF <miguelmonteagudoferia@gmail.com>
2025-02-07 10:52:10 +0100

    termine

--- Programacion/Práctica7/Ejercicio3.c
+++ Programacion/Práctica7/Ejercicio3.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
 
     if (argc == 2) {
         n = atoi(argv[1]); 
-        if (n < 0) {
+        if (n < 3) {
             printf("Error: El numero debe de ser mayor o igual 3. \n");
             return 1;
      	}
